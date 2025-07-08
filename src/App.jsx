8189bb80d200c4a1a3caefeb1272d162f96451c4
Nex <niko02xbox@gmail.com>
2025-07-08 13:59:59 +0100

    Added Taskbar items while opened and closed+bring to front works+added showOnDesktop so apps can be in start men but not desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
-// Importing drag-and-drop context provider from dnd-kit library
 import { DndContext } from '@dnd-kit/core';
-// Custom hook to manage windows' state and actions
 import useWindowManager from './objects/system32/useWindowManager.js';
-// UI components for taskbar and desktop
 import Taskbar from './objects/Taskbar/Taskbar.jsx';
 import Desktop from "./objects/Desktop/Desktop.jsx";
-// WindowManager component handles rendering and behavior of open windows
 import WindowManager from "./objects/system32/WindowManager.jsx";
 
-// Importing icon images for different apps/programs
+// Importing icon images
 import musicplayericon from "./objects/Desktop/img/icons/musicplayer.png";
 import settingsicon from "./objects/Desktop/img/icons/settings.png";
 import equalizericon from "./objects/Desktop/img/icons/equalizer.png";
 import aiicon from "./objects/Desktop/img/icons/aiicon.png";
 import cmdicon from "./objects/Desktop/img/icons/cmd.png";
-import notepadicon from "./objects/Desktop/img/icons/notepadicon.png"
-
-// Import BgEdit
-import BgEdit from './objects/system32/BgEdit.jsx';
+import notepadicon from "./objects/Desktop/img/icons/notepadicon.png";
+import bgediticon from "./objects/Desktop/img/icons/bgediticon.png";
 
 function App() {
-    // State for Desktop background color
-    const [desktopBgColor, setDesktopBgColor] = useState('#008C8A'); // default dark gray
-    const openBgEdit = () => {openWindow("bgedit")}
-    const openCalculator = () => {openWindow("calculator")}
+    const [desktopBgColor, setDesktopBgColor] = useState('#008C8A');
+    const openBgEdit = () => { openWindow("bgedit") };
+    const openCalculator = () => { openWindow("calculator") };
 
-    // Destructure states and handlers from custom window manager hook
     const {
         windowStates,
         repoUrl,
@@ -38,15 +30,31 @@ function App() {
         closeWindow,
     } = useWindowManager('https://api.github.com/repos/amberleafhash/toonStorage/contents/');
 
-    // List of program icons to display on desktop and taskbar, with click handlers to open windows
     const programIcons = [
-        { id: 1, name: "Settings", icon: settingsicon, onClick: () => openWindow("settings") },
-        { id: 2, name: "Music Player", icon: musicplayericon, onClick: () => openWindow("music") },
-        { id: 3, name: "Equalizer", icon: equalizericon, onClick: () => openWindow("equalizer") },
-        { id: 4, name: "Assistant", icon: aiicon, onClick: () => openWindow("assistant") },
-        { id: 5, name: "CMD", icon: cmdicon, onClick: () => openWindow("cmd") },
-        { id: 6, name: "Notepad", icon: notepadicon, onClick: () => openWindow("notepad") },
+        { id: 1, name: "Settings", icon: settingsicon, onClick: () => openWindow("settings"), showOnDesktop: true },
+        { id: 2, name: "Music Player", icon: musicplayericon, onClick: () => openWindow("music"), showOnDesktop: true },
+        { id: 3, name: "Equalizer", icon: equalizericon, onClick: () => openWindow("equalizer"), showOnDesktop: true }, // Not on desktop
+        { id: 4, name: "Assistant", icon: aiicon, onClick: () => openWindow("assistant"), showOnDesktop: true },  // Not on desktop
+        { id: 5, name: "CMD", icon: cmdicon, onClick: () => openWindow("cmd"), showOnDesktop: true },
+        { id: 6, name: "Notepad", icon: notepadicon, onClick: () => openWindow("notepad"), showOnDesktop: false },
+        { id: 7, name: "BGedit", icon: bgediticon, onClick: () => openWindow("bgedit"), showOnDesktop: false },
     ];
+
+    // Generate taskbar items based on open windows
+    const taskbarItems = Object.entries(windowStates)
+        .filter(([windowId, windowData]) => windowData.isOpen)
+        .map(([windowId]) => {
+            const programIcon = programIcons.find(
+                (icon) => icon.name.toLowerCase() === windowId.toLowerCase()
+            );
+
+            return {
+                id: windowId,
+                appTitle: programIcon ? programIcon.name : windowId,
+                icon: programIcon ? programIcon.icon : null,
+                onClick: () => bringToFront(windowId),
+            };
+        });
 
     return (
         <DndContext onDragEnd={handleDragEnd}>
@@ -60,16 +68,11 @@ function App() {
                     setDesktopBgColor={setDesktopBgColor}
                     openBgEdit={openBgEdit}
                     openCalculator={openCalculator}
-
-
                 />
-
-                {/* Pass background color to Desktop */}
-                <Desktop icons={programIcons} bgColor={desktopBgColor} />
-                <Taskbar icons={programIcons} />
-
-                {/* Background editor to control desktopBgColor */}
-
+                {/* Only show desktop icons marked with showOnDesktop */}
+                <Desktop icons={programIcons.filter(icon => icon.showOnDesktop)} bgColor={desktopBgColor} />
+                {/* Taskbar gets full list for start menu, etc. */}
+                <Taskbar icons={programIcons} taskbarItems={taskbarItems} windowStates={windowStates} />
             </div>
         </DndContext>
     );
--- src/objects/Taskbar/Taskbar.jsx
+++ src/objects/Taskbar/Taskbar.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Taskbar.css";
 import startbtnimg from "./img/startbutton.png";
 import startmenuimg from "./img/startMenuBarIcon.png";
 
-const Taskbar = ({ icons }) => {
+const Taskbar = ({ icons, taskbarItems = [] }) => {
     const [isStartMenuVisible, setStartMenuVisible] = useState(false);
 
     const toggleStartMenu = () => {
@@ -19,13 +19,31 @@ const Taskbar = ({ icons }) => {
             )}
             <div className="Taskbar">
                 <StartButton onClick={toggleStartMenu} />
+
+                {/* Dynamic TaskbarItems */}
+                {taskbarItems.map(({ id, appTitle, icon, onClick }) => (
+                    <TaskbarItem
+                        key={id}
+                        appTitle={appTitle}
+                        icon={icon}
+                        onClick={onClick}
+                    />
+                ))}
+
                 <TaskbarClock />
             </div>
         </div>
     );
 };
 
-
+const TaskbarItem = ({ appTitle, icon, onClick }) => {
+    return (
+        <div className="TaskbarItem" onClick={onClick}>
+            <div><img src={icon} alt={`${appTitle} icon`} /></div>
+            <div>{appTitle}</div>
+        </div>
+    );
+};
 
 const StartButton = ({ onClick }) => {
     return (
@@ -40,24 +58,21 @@ const TaskbarClock = () => {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            // Use Date object and format it using Intl.DateTimeFormat for better accuracy
             const formattedTime = new Intl.DateTimeFormat([], {
                 hour: '2-digit',
                 minute: '2-digit',
-                hour12: true, // This enables AM/PM formatting
+                hour12: true,
             }).format(new Date());
 
             setTime(formattedTime);
-        }, 60000); // Update the time every minute
+        }, 60000);
 
-        // Initial time setup
         setTime(new Intl.DateTimeFormat([], {
             hour: '2-digit',
             minute: '2-digit',
             hour12: true,
         }).format(new Date()));
 
-        // Clear the interval when the component is unmounted
         return () => clearInterval(timer);
     }, []);
 
@@ -72,7 +87,7 @@ const StartMenu = ({ icons }) => {
     return (
         <div className="StartMenu">
             <div className="leftBar">
-                <img className="TaskbarImg" src={startmenuimg} alt="image of button" />
+                <img className="TaskbarImg" src={startmenuimg} alt="Start Menu Icon" />
             </div>
             <div className="StartMenuContent">
                 {icons.map(({ id, name, icon, onClick }) => (
@@ -80,7 +95,7 @@ const StartMenu = ({ icons }) => {
                         key={id}
                         menuItemName={name}
                         StartMenuIcon={icon}
-                        onClick={onClick} // Pass the onClick handler
+                        onClick={onClick}
                     />
                 ))}
             </div>
@@ -90,12 +105,11 @@ const StartMenu = ({ icons }) => {
 
 const StartMenuItem = ({ menuItemName, StartMenuIcon, onClick }) => {
     return (
-        <div className="StartMenuItem" onClick={onClick}> {/* Attach the onClick handler */}
+        <div className="StartMenuItem" onClick={onClick}>
             <img className="StartMenuIcon" src={StartMenuIcon} alt={menuItemName} />
             <p>{menuItemName}</p>
         </div>
     );
 };
-
 
 export default Taskbar;
